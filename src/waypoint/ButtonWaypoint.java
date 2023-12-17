54249774ccf3d575e59de025d803646a1e6171bd
phong <nguyenduyphong_t67@hus.edu.vn>
2023-12-17 21:02:28 +0700

    update

--- src/waypoint/ButtonWaypoint.java
+++ src/waypoint/ButtonWaypoint.java
@@ -7,8 +7,8 @@ public class ButtonWaypoint extends JButton {
 
     public ButtonWaypoint() {
         setContentAreaFilled(false);
-        setIcon(new ImageIcon(getClass().getResource("/icon/pin3.png")));
+        setIcon(new ImageIcon(getClass().getResource("/icon/station4.png")));
         setCursor(new Cursor(Cursor.HAND_CURSOR));
-        setSize(new Dimension(24, 24));
+        setSize(new Dimension(30, 30));
     }
 }
